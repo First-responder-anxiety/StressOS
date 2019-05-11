8adb720b8ba4aed0625b53adbb45381c5f02e04d
John Sebastian <32150363+sebastianschroh@users.noreply.github.com>
2019-05-11 17:54:17 -0600

    added new icons

--- app/src/main/java/com/example/stressos/HomeActivity.java
+++ app/src/main/java/com/example/stressos/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.stressos;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
     private TextView mTextMessage;
+    private String username;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,13 +19,13 @@ public class HomeActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    mTextMessage.setText("Hello, " + username);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_badges:
+                    mTextMessage.setText(R.string.title_badges);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_questionnaire:
+                    mTextMessage.setText(R.string.title_questionnaire);
                     return true;
                 case R.id.navigation_settings:
                     mTextMessage.setText(R.string.title_settings);
@@ -40,6 +42,9 @@ public class HomeActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        Intent intent = getIntent();
+        username = intent.getStringExtra("USERNAME");
+        mTextMessage.setText("Hello, " + username);
     }
 
     @Override
