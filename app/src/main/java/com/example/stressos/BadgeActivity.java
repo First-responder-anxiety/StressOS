859421f7b4535a9df623fea311878db0a8560823
KarlaMariann Juego <karlamariann.juego@ucalgary.ca>
2019-05-12 15:47:27 -0600

    - Changed activity_badge.xml to include general textviews.

--- app/src/main/java/com/example/stressos/BadgeActivity.java
+++ app/src/main/java/com/example/stressos/BadgeActivity.java
@@ -8,8 +8,9 @@ public class BadgeActivity extends AppCompatActivity {
 
     // As of right now, there is a default longest streak and current streak.
     // Will have to create a function that calculates these two values or obtains them from the database.
-    private int userLongestStreak = 14;
-    private int userCurrentStreak = 5;
+    // Needs to be a STRING.
+    private String userLongestStreak = "14";
+    private String userCurrentStreak = "5";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +18,12 @@ public class BadgeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_badge);
 
         // Set the longest Streak number.
-        TextView longestStreakTextView = (TextView) findViewById(R.id.longestStreakNumber);
+        TextView longestStreakTextView = findViewById(R.id.longestStreakNumber);
         longestStreakTextView.setText(userLongestStreak);
 
         // Set the current Streak number.
-        TextView currentStreakTextView = (TextView) findViewById(R.id.currentStreakNumber);
-        longestStreakTextView.setText(userCurrentStreak);
+        TextView currentStreakTextView = findViewById(R.id.currentStreakNumber);
+        currentStreakTextView.setText(userCurrentStreak);
 
     }
 }
